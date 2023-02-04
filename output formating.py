5820c399a05a57b82dd426b2dcbbf67e255bcb05
SamarthaP77 <123925195+SamarthaP77@users.noreply.github.com>
2023-02-04 21:56:59 +0545

    Add files via upload

---
+++ output formating.py
@@ -0,0 +1,67 @@
+#Output Formatting
+#print integer and float value
+
+print("Rank:%d,Percentage:%f"%(1,97.8))
+
+#print integer
+print("Total students:%d, no of boys:%d"%(56,89))
+
+#Using modules
+print("Total students:%4d, percentage:%.3f"%(8777777,7890.78965))
+
+print("%d %s cost $%.2f" % (12, "apples", 3.44))
+
+welcome_sentence = "Hello, my website name is %s." % "JavaTpoint"  
+welcome_sentence   
+
+
+#Formatting output using format method
+print('I am learning {} from "{}!"'.format('football','coach'))
+
+print('{0} and {1}'.format('Geeks', 'Portal'))
+ 
+print('{1} and {0}'.format('Geeks', 'Portal'))
+
+#Formatting ouput using string method
+cstr = "I love geeksforgeeks"
+   
+# Printing the center aligned 
+# string with fillchr
+print ("Center aligned string with fillchr: ")
+print (cstr.center(40, '#'))
+ 
+# Printing the left aligned 
+# string with "-" padding 
+print ("The left aligned string is : ")
+print (cstr.ljust(40, '-'))
+ 
+# Printing the right aligned string
+# with "-" padding 
+print ("The right aligned string is : ")
+print (cstr.rjust(40, '-'))
+
+#Arthimetic operators
+a = 8
+b = 9
+print("Addition = ", a+b)
+print("Multiplication = %d" %(a*b))
+print("Division = {}".format(a/b))
+
+#Relational operators
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#Assignment operators
+x = 10
+print(10)
+
+
+
+ 
+
+
+      
+
