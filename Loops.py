2b3515a267d064729d13e59b7e12dcd22b3f706f
SamarthaP77 <123925195+SamarthaP77@users.noreply.github.com>
2023-02-08 23:14:22 +0545

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -1,63 +1,71 @@
-#Forloop
+#Updating Lists
+z = [88,90,67,67,90]
+#Update the item at index with the string "Samartha"
+z[1] = "Samartha"
+print(z)
+#Appending or adding a new value to a list
+z.append('football')
+print(z)
 
-for letter in 'Basketball':
- print('Current letter is: ', letter)
-
-for z in range(20):
-    print("Football")
-
-#whileloop
-count = 0
-while count <10:
-    print("The count is", count)
-    count = count+1
-
-food = 10
-while food<90:
-    print('food')
-    food+=10
-for a in range(8,60,8):
-    print(a)
-
-#Nestedloop
-for i in range(1,6):
-  for j in range (1,i+1):
-      print (j, end=" ")
-  print()
-
-for i in range(1,6):
-    for j in range(1,5):
-        print("football")
+#Deleting list elements
+#Using del statement
+a = ["Python",100,"football",2,"is"]
+del a[0]
+print(a)
 
 
-#Break statement
-for i in range(20,40):
-    if i==35:
-        break
-    print(i)
+#Basic List Operators
+a = [7,8,9,0,6]
+b = [9,8,6,5,6]
+print(a + b)
+print(a*4)
+print(6 in b)
+print(9 not in a)
+print(len(a))
+print(max(b))
+print(min(a))
 
-#Continue statement
-for j in range(6,20):
-    if j==17:
-        continue
-    print(j)
+#Built-in List Functions and Methods
 
-#Pass statement
-for a in range(6,20):
-    if a==17:
-        pass
-    print(a)
-
-#List
-    #Accesing values in list
-L=[9,18.8,'Hi',15.99,'School',100]
-print(L[5])
-print(L[2:4])
-print(L[-2])
-print(L[3])
+d = [10,15,20,'Hello','Python',10]
+d.append('Hello')
+print(d)
+print(d.count(10))
+e = [900,800]
+d.extend(e)
+print(d)
+print(d.index('Hello'))
+d.insert(2,"ok")
+print(d)
+print(d.pop())
+print(d.pop(3))
+print(d)
+d.reverse()
+print(d)
 
 
-    
-    
+#Write a python program to sum all the items in a list
+def sum_list(items):
+    sum_numbers = 0
+    for x in items:
+        sum_numbers += x
+    return sum_numbers
+print(sum_list([1,2,-8]))
 
+
+#Replace list value with a new item if found
+list1 = [5, 10, 15, 20, 25, 50, 20]
+index = list1.index(20)
+list1[index] = 200
+print(list1)
+
+#Tuples
+T = (99,18,'Hi',12,"Hello",15.55,'Pragramming',100,125.5)
+print(T[5])
+print(T[1:5])
+print(T[2:8])
+print(T[2:9:3])
+print(T[-1])
+print(T[-5])
+ 
 
